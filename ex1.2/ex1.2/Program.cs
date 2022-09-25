2fb309f93a6a3587cf8021ad2c3c259cdd936220
Кюлян Королина <114185536+KCorolina@users.noreply.github.com>
2022-09-25 18:14:14 +0400

    Add files via upload

---
+++ ex1.2/ex1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число строк");
+            string str = Console.ReadLine();
+            int rowCount = int.Parse(str);
+            var star = "*";
+            if(rowCount > 0)
+            {
+                while (star.Length <= rowCount)
+                {
+                    Console.WriteLine(star);
+                    star += "*";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный параметр");
+            }
+        }
+    }
+}
