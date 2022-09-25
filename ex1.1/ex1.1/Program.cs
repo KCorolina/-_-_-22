fb1faa6444043b55c3d41285abacf5f9e906a43c
Кюлян Королина <114185536+KCorolina@users.noreply.github.com>
2022-09-25 15:25:43 +0400

    Add files via upload

---
+++ ex1.1/ex1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ex1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину и щирину прямоугольникв");
+            string str = Console.ReadLine();
+            double length = double.Parse(str);
+            string str1 = Console.ReadLine();
+            double width = double.Parse(str1);
+            if (length < 0 || width < 0)
+            {
+                Console.WriteLine("Неверные параметры");
+            }
+            else
+            {
+                double Square = length * width;
+                Console.Write(Square);
+            }
+        }
+    }
+}
